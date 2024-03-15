337de070a1b38b427ef346b01a8a2959968f47cb
Walid Mostefaoui <pro.walid.mostefaoui@gmail.com>
2024-03-15 23:31:45 +0100

    refactor(eslint-config): use of new version eslint-config-mkw

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,7 +1,3 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
 /*
  * This is a custom ESLint configuration for use with
  * internal (bundled by their consumer) libraries
@@ -14,30 +10,14 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    browser: true,
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
-  overrides: [
-    // Force ESLint to detect .tsx files
-    { files: ["*.js?(x)", "*.ts?(x)"] },
-  ],
-};
+	extends: [
+		require.resolve('eslint-config-mkw/react-library'),
+		'eslint-config-turbo',
+	],
+	ignorePatterns: [
+		// Ignore dotfiles
+		'.*.js',
+		'node_modules/',
+		'dist/',
+	],
+}
